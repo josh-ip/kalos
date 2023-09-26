b29a734b50fb56ee4268bfc440d49d61c3883c1e
Josh <joshua.ip@yale.edu>
2023-09-26 07:48:07 -0700

    update website name to Kalos

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,7 +12,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Next.js OpenAI Template</title>
+        <title>Kalos</title>
         <meta
           name="description"
           content="Next.js Template for building OpenAI applications with Supabase."
